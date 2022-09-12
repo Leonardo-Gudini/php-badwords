ff1056225e8607cabef6df65a4fe1900b91adccf
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-09-12 10:01:22 +0200

    fix: corretto esercizio

--- index.php
+++ index.php
@@ -8,18 +8,16 @@
 </head>
 <body>
     
-    <?php 
+    <?php
         $wordReceived = $_POST['word'];
 
-        $uncensoredtext = 'La Parolaccia è ' . ' ' . $wordReceived;
+        $text = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
 
-        $badword = str_replace($wordReceived, '***', $wordReceived);
-        
-        $censoredtext = 'La Parolaccia è ' . ' ' . $badword;
+        $textcensored = str_ireplace($wordReceived, '***', $text);
 
-        $length = strlen($uncensoredtext);
+        $length = strlen($text);
 
-        echo $censoredtext . '<br>';
+        echo $textcensored . '<br>';
 
         echo 'La frase è lunga: ' . $length;
     ?>
